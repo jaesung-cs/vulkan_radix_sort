75716739b097062355e6221fa4d8a8e6c64c7a13
Jaesung Park <jaesung.cs@gmail.com>
2025-01-03 23:29:26 +0900

    print wrong answer

--- bench/bench.cc
+++ bench/bench.cc
@@ -20,6 +20,7 @@ int main(int argc, char** argv) {
 
   try {
     auto benchmark = BenchmarkFactory::Create(type);
+    auto cpu_benchmark = BenchmarkFactory::Create("cpu");
     // TODO: provide seed
     DataGenerator data_generator;
 
@@ -28,12 +29,31 @@ int main(int argc, char** argv) {
       auto data = data_generator.Generate(size);
 
       auto result0 = benchmark->Sort(data.keys);
-      {
-        double perf = (static_cast<double>(size) / 1e9) /
-                      (static_cast<double>(result0.total_time) / 1e9);
-        std::cout << "total time: "
-                  << static_cast<double>(result0.total_time) / 1e6 << "ms ("
-                  << perf << " GItems/s)" << std::endl;
+      double perf = (static_cast<double>(size) / 1e9) /
+                    (static_cast<double>(result0.total_time) / 1e9);
+      std::cout << "total time: "
+                << static_cast<double>(result0.total_time) / 1e6 << "ms ("
+                << perf << " GItems/s)" << std::endl;
+
+      auto result1 = cpu_benchmark->Sort(data.keys);
+      for (int i = 0; i < data.keys.size(); ++i) {
+        if (result0.keys[i] != result1.keys[i]) {
+          std::cout << "wrong key at index " << i << std::endl;
+
+          int i0 = std::max(i - 5, 0);
+          int i1 = std::min<int>(i + 6, data.keys.size());
+          std::cout << "keys   (" << std::setw(6) << type << "): ";
+          for (int j = i0; j < i1; ++j) {
+            std::cout << std::setw(9) << std::hex << data.keys[j];
+          }
+          std::cout << std::endl;
+          std::cout << "keys   (answer): ";
+          for (int j = i0; j < i1; ++j) {
+            std::cout << std::setw(9) << std::hex << result1.keys[j];
+          }
+          std::cout << std::endl;
+          break;
+        }
       }
     }
 
@@ -42,12 +62,73 @@ int main(int argc, char** argv) {
                 << std::endl;
       auto data = data_generator.Generate(size);
       auto result0 = benchmark->SortKeyValue(data.keys, data.values);
-      {
-        double perf = (static_cast<double>(size) / 1e9) /
-                      (static_cast<double>(result0.total_time) / 1e9);
-        std::cout << "total time: "
-                  << static_cast<double>(result0.total_time) / 1e6 << "ms ("
-                  << perf << " GItems/s)" << std::endl;
+
+      double perf = (static_cast<double>(size) / 1e9) /
+                    (static_cast<double>(result0.total_time) / 1e9);
+      std::cout << "total time: "
+                << static_cast<double>(result0.total_time) / 1e6 << "ms ("
+                << perf << " GItems/s)" << std::endl;
+
+      auto result1 = cpu_benchmark->SortKeyValue(data.keys, data.values);
+      for (int i = 0; i < data.keys.size(); ++i) {
+        if (result0.keys[i] != result1.keys[i]) {
+          std::cout << "wrong key at index " << i << std::endl;
+
+          int i0 = std::max(i - 5, 0);
+          int i1 = std::min<int>(i + 6, data.keys.size());
+          std::cout << "keys   (" << std::setw(6) << type << "): ";
+          for (int j = i0; j < i1; ++j) {
+            std::cout << std::setw(9) << std::hex << result0.keys[j];
+          }
+          std::cout << std::endl;
+          std::cout << "keys   (answer): ";
+          for (int j = i0; j < i1; ++j) {
+            std::cout << std::setw(9) << std::hex << result1.keys[j];
+          }
+          std::cout << std::endl;
+          std::cout << "values (" << std::setw(6) << type << "): ";
+          for (int j = i0; j < i1; ++j) {
+            std::cout << std::setw(9) << std::hex << result0.values[j];
+          }
+          std::cout << std::endl;
+          std::cout << "values (answer): ";
+          for (int j = i0; j < i1; ++j) {
+            std::cout << std::setw(9) << std::hex << result1.values[j];
+          }
+          std::cout << std::endl;
+          break;
+        }
+      }
+
+      for (int i = 0; i < data.keys.size(); ++i) {
+        if (result0.values[i] != result1.values[i]) {
+          std::cout << "wrong value at index " << i << std::endl;
+
+          int i0 = std::max(i - 5, 0);
+          int i1 = std::min<int>(i + 6, data.keys.size());
+          std::cout << "keys   (" << std::setw(6) << type << "): ";
+          for (int j = i0; j < i1; ++j) {
+            std::cout << std::setw(9) << std::hex << result0.keys[j];
+          }
+          std::cout << std::endl;
+          std::cout << "keys   (answer): ";
+          for (int j = i0; j < i1; ++j) {
+            std::cout << std::setw(9) << std::hex << result1.keys[j];
+          }
+          std::cout << std::endl;
+          std::cout << "values (" << std::setw(6) << type << "): ";
+          for (int j = i0; j < i1; ++j) {
+            std::cout << std::setw(9) << std::hex << result0.values[j];
+          }
+          std::cout << std::endl;
+          std::cout << "values (answer): ";
+          for (int j = i0; j < i1; ++j) {
+            std::cout << std::setw(9) << std::hex << result1.values[j];
+          }
+          std::cout << std::endl;
+          std::cout << std::endl;
+          break;
+        }
       }
     }
 
