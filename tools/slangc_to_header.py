6f3c40878bf49ed4fd8f717b71df116d3e6b941a
Jaesung Park <jaesung.cs@gmail.com>
2025-10-23 16:12:48 +0900

    fix

--- tools/slangc_to_header.py
+++ tools/slangc_to_header.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
   stdout, stderr = proc.stdout, proc.stderr
 
   if not os.path.exists(path):
-    raise RuntimeError("\n".join([f"Output {path} not found", "stdout:", stdout.decode() if stdout else None, "stderr:", stderr.decode() if stderr else None]))
+    raise RuntimeError("\n".join([f"Output {path} not found", "stdout:", stdout.decode() if stdout else "None", "stderr:", stderr.decode() if stderr else "None"]))
 
   with open(path, "rb") as f:
     spv = f.read()
